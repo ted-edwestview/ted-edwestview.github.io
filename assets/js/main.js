fee0be855ebd14dfdf899d2ac35b94c464e97d0b
nLevin13 <nyro9999@gmail.com>
2017-07-28 12:55:08 -0700

    Ishi's new website 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,25 @@
 /*
-	Strata by HTML5 UP
+	Read Only by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	var settings = {
-
-		// Parallax background effect?
-			parallax: true,
-
-		// Parallax factor (lower = more intense, higher = less intense).
-			parallaxFactor: 20
-
-	};
-
 	skel.breakpoints({
-		xlarge: '(max-width: 1800px)',
+		xlarge: '(max-width: 1680px)',
 		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
+		medium: '(max-width: 1024px)',
 		small: '(max-width: 736px)',
 		xsmall: '(max-width: 480px)'
 	});
 
 	$(function() {
 
-		var $window = $(window),
-			$body = $('body'),
+		var $body = $('body'),
 			$header = $('#header'),
-			$footer = $('#footer'),
-			$main = $('#main');
-
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
-
-		// Touch?
-			if (skel.vars.mobile) {
-
-				// Turn on touch mode.
-					$body.addClass('is-touch');
-
-				// Height fix (mostly for iOS).
-					window.setTimeout(function() {
-						$window.scrollTop($window.scrollTop() + 1);
-					}, 0);
-
-			}
+			$nav = $('#nav'), $nav_a = $nav.find('a'),
+			$wrapper = $('#wrapper');
 
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
@@ -63,72 +32,81 @@
 				);
 			});
 
-		// Footer.
-			skel.on('+medium', function() {
-				$footer.insertAfter($main);
-			});
-
-			skel.on('-medium !medium', function() {
-				$footer.appendTo($header);
-			});
-
 		// Header.
+			var ids = [];
 
-			// Parallax background.
+			// Set up nav items.
+				$nav_a
+					.scrolly({ offset: 44 })
+					.on('click', function(event) {
 
-				// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-					if (skel.vars.browser == 'ie'
-					||	skel.vars.mobile)
-						settings.parallax = false;
+						var $this = $(this),
+							href = $this.attr('href');
 
-				if (settings.parallax) {
+						// Not an internal link? Bail.
+							if (href.charAt(0) != '#')
+								return;
 
-					skel.on('change', function() {
+						// Prevent default behavior.
+							event.preventDefault();
 
-						if (skel.breakpoint('medium').active) {
+						// Remove active class from all links and mark them as locked (so scrollzer leaves them alone).
+							$nav_a
+								.removeClass('active')
+								.addClass('scrollzer-locked');
 
-							$window.off('scroll.strata_parallax');
-							$header.css('background-position', 'top left, center center');
+						// Set active class on this link.
+							$this.addClass('active');
 
-						}
-						else {
+					})
+					.each(function() {
 
-							$header.css('background-position', 'left 0px');
+						var $this = $(this),
+							href = $this.attr('href'),
+							id;
 
-							$window.on('scroll.strata_parallax', function() {
-								$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-							});
+						// Not an internal link? Bail.
+							if (href.charAt(0) != '#')
+								return;
 
-						}
+						// Add to scrollzer ID list.
+							id = href.substring(1);
+							$this.attr('id', id + '-link');
+							ids.push(id);
 
 					});
 
-					$window.on('load', function() {
-						$window.triggerHandler('scroll');
+			// Initialize scrollzer.
+				$.scrollzer(ids, { pad: 300, lastHack: true });
+
+		// Off-Canvas Navigation.
+
+			// Title Bar.
+				$(
+					'<div id="titleBar">' +
+						'<a href="#header" class="toggle"></a>' +
+						'<span class="title">' + $('#logo').html() + '</span>' +
+					'</div>'
+				)
+					.appendTo($body);
+
+			// Header.
+				$('#header')
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						hideOnSwipe: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'right',
+						target: $body,
+						visibleClass: 'header-visible'
 					});
 
-				}
-
-		// Main Sections: Two.
-
-			// Lightbox gallery.
-				$window.on('load', function() {
-
-					$('#two').poptrox({
-						caption: function($a) { return $a.next('h3').text(); },
-						overlayColor: '#2c2c2c',
-						overlayOpacity: 0.85,
-						popupCloserText: '',
-						popupLoaderText: '',
-						selector: '.work-item a.image',
-						usePopupCaption: true,
-						usePopupDefaultStyling: false,
-						usePopupEasyClose: false,
-						usePopupNav: true,
-						windowMargin: (skel.breakpoint('small').active ? 0 : 50)
-					});
-
-				});
+			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+					$('#titleBar, #header, #wrapper')
+						.css('transition', 'none');
 
 	});
 
